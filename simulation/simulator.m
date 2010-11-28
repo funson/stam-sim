12973db2deda7dd24edf527173901b912333f5cd
neilrqm <neilrqm@gmail.com>
2010-11-28 02:41:43 +0000

    Fixed up some bugs, seems to work now with the demo schedule.

--- simulation/simulator.m
+++ simulation/simulator.m
@@ -1,6 +1,10 @@
 %Simulation Framework
 %November 26th, 2010
 
+%rand('seed', 0);       % initialize rand to known seed
+%randn('seed', 0);      % initialize randn to known seed
+clear functions;        % clear persistent values in functions
+
 %set endpoint for simulation
 simEnd = 20;
 
@@ -18,51 +22,59 @@ batteryHistory(1) = currentBatteryLevel;
 idleEnergy = 0.3;
 
 %create taskSet Table
-taskSet = [10, 1.5, 3
-           21, 4.0, 2];
+%column 1 energy consumed per time unit, column 2 task duration
+taskSet = [1.0, 3
+           1.5, 2];
 
 %static schedule table
 %column 1 time, column 2 task#
-scheduleTable = [   2,1
-                    5,2
+scheduleTable = [   3,1
+                    6,2
                     13,1
-                    16,1];
-
+                    16,2];
 
 %current executing task. If 0 --> idle
 curTask = 0;
 %finish point of the current task
-taskEnd = 0;
+taskEnd = -1;
 
 %set initial row index of scheduleTable
 scheduleIndex = 1;
 
-for t = 1 : simEnd
+for t = 0 : simEnd
     if (t == scheduleTable(scheduleIndex,1))
         curTask = scheduleTable(scheduleIndex,2);
-        taskEnd =  t + taskSet(curTask,3);
+        taskEnd =  t + taskSet(curTask,2) - 1;
     end
     
+    
     if (curTask == 0)
-        currentBatteryLevel = updateBatteryLevel(currentBatteryLevel, idleEnergy);
+        energyConsumed = idleEnergy;
     else
-       currentBatteryLevel = updateBatteryLevel(currentBatteryLevel, taskSet(curTask,2)); 
+        energyConsumed = taskSet(curTask,1); 
     end
+    [currentBatteryLevel, radiation] = updateBatteryLevel(currentBatteryLevel, energyConsumed);
     
     %if there is no energy available, there is a violation
     if currentBatteryLevel < 0
         numViolations = numViolations + 1;
     end
     
-    %check to see if the current task has completed
+    s = sprintf('%5d - Running task %d, using energy %.2f (%.2f remaining, charged from irradiance of %.2f W/m2)', t, curTask, energyConsumed, currentBatteryLevel, radiation);
+    disp(s)
+    
+    
     if (taskEnd == t)
         curTask = 0;
+        scheduleIndex = mod(scheduleIndex, size(scheduleTable, 1)) + 1;
     end
     
     %update the battery level history
-    batteryHistory(t) = currentBatteryLevel;
+    batteryHistory(t+1) = currentBatteryLevel;
     
-    %print the battery history level
-    batteryHistory
-  
+    %print the battery history level  
 end
+disp(' ')
+x = 0 : size(batteryHistory, 2) - 1;
+plot(x, batteryHistory);
+axis([0 simEnd 0 12])
