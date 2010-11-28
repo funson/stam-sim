55a39481c9cdc38a933ea884dafc44ea71ed9ffb
neilrqm <neilrqm@gmail.com>
2010-11-28 22:39:57 +0000

    - generateTaskSet.m: Added function for generating task sets. - simulate.m: simulation now returns upon the first energy violation - simulate.m: added parameter for quiet operation (set to 1 when calling simulate() in loops) - simulate.m: clock now starts at t = 1 so that energy graph starts at full capacity (no task can start at t=0, is this desirable?). - run.m: modified some stuff to work with new simulate and generateTaskSet functions.

--- simulation/run.m
+++ simulation/run.m
@@ -1,23 +1,29 @@
-%create taskSet Table
-%column 1 energy consumed per time unit, column 2 task duration
-taskSet = [1.0, 3
-           1.5, 2];
+% create taskSet Table
+% column 1 is energy consumed per time unit, column 2 task duration
+taskSet = generateTaskSet(2);
 
-%set some value for idle task energy demand
-idleEnergy = 0.2;
+% set some value for idle task energy demand
+idleEnergy = 0.1;
 
 % set initial battery level
 batteryLevel = 12;
 
-%static schedule table
-%column 1 time, column 2 task#
+% static schedule table
+% column 1 is start time, column 2 task number
 scheduleTable = [   3,1
                     6,2
                     13,1
                     16,2];
 
-%set endpoint for simulation
+% set end time for simulation (this can be calculated
 simEnd = 40;
 
+% keep track of the number of violations that have occurred in a
+% simulation.
+numViolations = 0;
+
+for i = 1 : 1
 % simulate the calculated schedule
-simulate(taskSet, scheduleTable, simEnd, batteryLevel, idleEnergy);
+numViolations = numViolations + simulate(taskSet, scheduleTable, simEnd, batteryLevel, idleEnergy, 0);
+end
+numViolations
