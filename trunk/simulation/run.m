bda8639f3e06dfab89c52e5ae9a391e864d3a246
neilrqm@gmail.com <neilrqm@gmail.com@5a73a3e8-56d7-28e9-a24d-b86c51e2a46f>
2010-12-06 08:41:04 +0000

    Changed code to convert STAM schedule back to real equivalent.  Now it sucks!

--- trunk/simulation/run.m
+++ trunk/simulation/run.m
@@ -1,21 +1,23 @@
 numRuns = 1;
-numSimulations = 1;
+numEdfSimulations = 1;
+numLsaSimulations = 0;
+numEdfStamSimulations = 1;
+numLsaStamSimulations = 0;
 
 edfViolationHistory = zeros(numRuns + 1,1);
-stamViolationHistory = zeros(numRuns + 1, 1);
+edfStamViolationHistory = zeros(numRuns + 1, 1);
+lsaViolationHistory = zeros(numRuns + 1,1);
+lsaStamViolationHistory = zeros(numRuns + 1, 1);
 for j = 1 : numRuns
-seed = j + 1;
+seed = j + 2009;
 
 rand('seed', seed);       % initialize rand to known seed
 randn('seed', seed);      % initialize randn to known seed
 clear functions;        % clear persistent values in functions
 
-% create taskSet Table
-% column 1 is energy consumed per time unit, column 2 task duration
-taskList = generateTaskList(4);
 
 % set some value for idle task energy demand
-idleEnergy = 0.1;
+idleEnergy = 0.05;
 
 % set initial battery level
 batteryLevel = 12;
@@ -23,36 +25,64 @@ batteryLevel = 12;
 % end time for simulation
 simEnd = 100;
 
-% static schedule table
-% column 1 is start time, column 2 task number
-scheduleTable = scheduleEDF(taskList, simEnd);
+% create real task list and schedule the real tasks
+[taskList, stamTasks] = generateTaskList(4);
+edfSchedule = scheduleEDF(taskList, simEnd);
+lsaSchedule = scheduleLSA(taskList, simEnd);
 
-%create STAM task set and create schedule table
-stamTasks = createSTAM(taskList);
-stamSchedule = scheduleEDF(stamTasks, simEnd);
+%create STAM task list and create schedule the virtual tasks
+edfStamSchedule = scheduleEDF(stamTasks, simEnd);%convertSTAM(taskList, stamTasks, scheduleEDF(stamTasks, simEnd));
+lsaStamSchedule = convertSTAM(taskList, stamTasks, scheduleLSA(stamTasks, simEnd));
 
 % keep track of the number of violations that have occurred in a
 % simulation.
-numViolations = 0;
-stamViolations = 0;
+edfViolations = 0;
+lsaViolations = 0;
+edfStamViolations = 0;
+lsaStamViolations = 0;
+
+% seed each simulation with the next random number
 seed = rand();
+
+%%%%% Run EDF simulation
 rand('seed', seed);       % initialize rand to known seed
 randn('seed', seed);      % initialize randn to known seed
-for i = 1 : numSimulations
+for i = 1 : numEdfSimulations
     % simulate the calculated schedule
-    [v, lastBatteryHistory] = simulate(taskList, scheduleTable, simEnd, batteryLevel, idleEnergy);
-    numViolations = numViolations + v;
+    [v, lastBatteryHistory] = simulate(taskList, edfSchedule, simEnd, batteryLevel, idleEnergy);
+    edfViolations = edfViolations + v;
 end
+
+%%%%% Run LSA simulation
 rand('seed', seed);       % initialize rand to known seed
 randn('seed', seed);      % initialize randn to known seed
-for i = 1 : numSimulations
-    [v, lastBatteryHistory] = simulate(stamTasks, stamSchedule, simEnd, batteryLevel, idleEnergy);
-    stamViolations = stamViolations + v;
+for i = 1 : numLsaSimulations
+    % simulate the calculated schedule
+    [v, lastBatteryHistory] = simulate(taskList, lsaSchedule, simEnd, batteryLevel, idleEnergy);
+    lsaViolations = lsaViolations + v;
+end
+
+%%%%% Run EDF STAM simulation
+rand('seed', seed);       % initialize rand to known seed
+randn('seed', seed);      % initialize randn to known seed
+for i = 1 : numEdfStamSimulations
+    [v, lastBatteryHistory] = simulate(taskList, edfStamSchedule, simEnd, batteryLevel, idleEnergy);
+    edfStamViolations = edfStamViolations + v;
+end
+
+%%%%% Run LSA STAM simulation
+rand('seed', seed);       % initialize rand to known seed
+randn('seed', seed);      % initialize randn to known seed
+for i = 1 : numLsaStamSimulations
+    [v, lastBatteryHistory] = simulate(taskList, lsaStamSchedule, simEnd, batteryLevel, idleEnergy);
+    lsaStamViolations = lsaStamViolations + v;
 end
 %numViolations
 %stamViolations
-edfViolationHistory(j) = numViolations;
-stamViolationHistory(j) = stamViolations;
+edfViolationHistory(j) = edfViolations;
+edfStamViolationHistory(j) = edfStamViolations;
+lsaViolationHistory(j) = lsaViolations;
+lsaStamViolationHistory(j) = lsaStamViolations;
 
-plotSimulation(taskList, scheduleTable, stamTasks, stamSchedule, lastBatteryHistory);
+plotSimulation(taskList, edfSchedule, stamTasks, edfStamSchedule, lastBatteryHistory);
 end
