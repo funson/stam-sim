9e14e53eb58d9fbe786cc90da88da9d7e379e397
dmarinak <dmarinak@5a73a3e8-56d7-28e9-a24d-b86c51e2a46f>
2010-12-14 20:40:18 +0000

    code to generate a plot of violations vs cpu utilization

---
+++ trunk/simulation/run_genSavedData.m
@@ -0,0 +1,126 @@
+clear all
+close all
+
+SESSION_NUMBER = 2;
+
+numRuns = 10;
+numSims = 100;
+numEdfSimulations = numSims;
+numLsaSimulations = numSims;
+numEdfStamSimulations = numSims;
+numLsaStamSimulations = numSims;
+
+edfViolationHistory = zeros(numRuns + 1,1);
+edfStamViolationHistory = zeros(numRuns + 1, 1);
+lsaViolationHistory = zeros(numRuns + 1,1);
+lsaStamViolationHistory = zeros(numRuns + 1, 1);
+
+% set some value for idle task energy demand
+idleEnergy = 0.05;
+
+% set initial battery level
+batteryLevel = 12;
+
+% end time for simulation
+simEnd = 100;
+
+disp(sprintf('SESSION_NUMBER: %d, running simulation with %d runs, %d numSims, %d sim end time',SESSION_NUMBER, numRuns, numSims, simEnd));
+
+tic
+for j = 1 : numRuns
+  
+  disp(sprintf('run %d',j)); 
+
+
+  %rand('seed', seed);       % initialize rand to known seed
+  %randn('seed', seed);      % initialize randn to known seed
+  %clear functions;        % clear persistent values in functions
+
+  % create real task list and schedule the real tasks
+  if j == 48
+      j;
+  end
+  %[taskList, stamTasks] = generateTaskList(4);
+
+  utilizationBinArray = [ 0.2, 0.3, 0.4, 0.5, 0.6];
+  utilizationBinWidth = 0.1;
+
+  for utilizationIndex = 1:length( utilizationBinArray ) 
+    utilizationBin = utilizationBinArray(utilizationIndex);
+
+    seed = j + utilizationIndex*numRuns
+
+    % seed each simulation with the next random number
+    %seed = rand();
+
+    %generate a task list within some utilization bound
+    [taskList, stamTasks] = generateTaskListBounded(4,utilizationBin,utilizationBin+utilizationBinWidth);
+
+    if numLsaSimulations > 0 || numLsaStamSimulations > 0
+        %use pseudoSimulate to use energy predictive ALAP algorithm
+        lsaSchedule = pseudoSimulate(taskList, scheduleALAP(taskList, simEnd), batteryLevel, idleEnergy, 0);
+
+        %lsaSchedule = scheduleALAP(taskList, simEnd);
+        lsaStamSchedule = pseudoSimulate(taskList, convertSTAM(taskList, stamTasks, scheduleALAP(stamTasks, simEnd)), simEnd, batteryLevel, idleEnergy);
+    end
+
+    edfSchedule = scheduleEDF(taskList, simEnd);
+
+    %create STAM task list and schedule the virtual tasks
+    edfStamSchedule = convertSTAM(taskList, stamTasks, scheduleEDF(stamTasks, simEnd));
+
+    % keep track of the number of violations that have occurred in a
+    % simulation.
+    edfViolations = 0;
+    lsaViolations = 0;
+    edfStamViolations = 0;
+    lsaStamViolations = 0;
+
+
+    %%%%% Run EDF simulation
+    rand('seed', seed);       % initialize rand to known seed
+    randn('seed', seed);      % initialize randn to known seed
+    for i = 1 : numEdfSimulations
+        % simulate the calculated schedule
+        [v, lastBatteryHistory] = simulate(taskList, edfSchedule, simEnd, batteryLevel, idleEnergy, 0);
+        edfViolations = edfViolations + v;
+    end
+
+    %%%%% Run LSA simulation
+    rand('seed', seed);       % initialize rand to known seed
+    randn('seed', seed);      % initialize randn to known seed
+    for i = 1 : numLsaSimulations
+        % simulate the calculated schedule
+        [v, lastBatteryHistory] = simulate(taskList, lsaSchedule, simEnd, batteryLevel, idleEnergy, 1);
+        lsaViolations = lsaViolations + v;
+    end
+
+    %%%%% Run EDF STAM simulation
+    rand('seed', seed);       % initialize rand to known seed
+    randn('seed', seed);      % initialize randn to known seed
+    for i = 1 : numEdfStamSimulations
+        [v, lastBatteryHistory] = simulate(taskList, edfStamSchedule, simEnd, batteryLevel, idleEnergy, 0);
+        edfStamViolations = edfStamViolations + v;
+    end
+
+    %%%%% Run LSA STAM simulation
+    rand('seed', seed);       % initialize rand to known seed
+    randn('seed', seed);      % initialize randn to known seed
+    for i = 1 : numLsaStamSimulations
+        [v, lastBatteryHistory] = simulate(taskList, lsaStamSchedule, simEnd, batteryLevel, idleEnergy, 1);
+        lsaStamViolations = lsaStamViolations + v;
+    end
+    
+    % save data from this run
+    pwrUsage(j,utilizationIndex) = sum((taskList(:,3).*  taskList(:,2)) ./ taskList(:,1));
+    edfViolationHistory(j,utilizationIndex) = edfViolations;
+    edfStamViolationHistory(j,utilizationIndex) = edfStamViolations;
+    lsaViolationHistory(j,utilizationIndex) = lsaViolations;
+    lsaStamViolationHistory(j,utilizationIndex) = lsaStamViolations;
+  end
+end
+time = toc;
+
+disp(sprintf('SAVING as SESSION_NUMBER: %d',SESSION_NUMBER));
+save(sprintf('run_%d',SESSION_NUMBER));
+
